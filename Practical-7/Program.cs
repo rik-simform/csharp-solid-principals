50e995c2ce33aec84f5e56fb1f32d438e3bf6c82
Rikesh Bhakta <rikesh.b@simformsolutions.com>
2022-03-31 10:49:12 +0530

    Adding Practical-7

---
+++ Practical-7/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Practical_7;
+
+DIP.project p = new DIP.project();
+p.implement(new DIP.BackEndDeveloper());
+p.implement(new DIP.FrontEndDeveloper());
